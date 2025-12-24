8e3f5857d164259d15539012cdb93065ecc1c95b
spayker <spykerstar@gmail.com>
2025-12-24 15:32:55 +0100

    fixed case sensitive coin param set, updated request param list to keep symbols optional

--- src/main/java/com/spayker/crypto/analysis/controller/CorrelationController.java
+++ src/main/java/com/spayker/crypto/analysis/controller/CorrelationController.java
@@ -23,16 +23,16 @@ public class CorrelationController {
 
     @GetMapping("/{targetCoin}/correlations")
     public ResponseEntity<?> fetchCandleHistoryByPairName(
-            @PathVariable @Pattern(regexp = "[A-Z]{3,10}") String targetCoin,
-            @RequestParam List<String> symbols,
+            @PathVariable @Pattern(regexp = "[a-zA-Z]{1,10}") String targetCoin,
+            @RequestParam(required = false) List<String> symbols,
             @RequestParam String startDateTime,
             @RequestParam String endDateTime,
             @RequestParam String stableCoin) {
 
         Instant start = Instant.parse(startDateTime);
         Instant end   = Instant.parse(endDateTime);
-        CorrelationRequest request = new CorrelationRequest(symbols, start, end, stableCoin);
+        CorrelationRequest request = new CorrelationRequest(symbols, start, end, stableCoin.toUpperCase());
 
-        return ResponseEntity.ok(correlationManager.getCorrelation(targetCoin, request));
+        return ResponseEntity.ok(correlationManager.getCorrelation(targetCoin.toUpperCase(), request));
     }
 }
